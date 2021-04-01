31c36e536d09ee5d71c03ee67947703cbbae8c4c
Zach Young <zacharysyoung@gmail.com>
2021-03-31 17:11:35 -0700

    added ViewPort

--- SwiftExplorer/Explorations/AffineTransforms.swift
+++ SwiftExplorer/Explorations/AffineTransforms.swift
@@ -11,20 +11,22 @@
 import SwiftUI
 import hexdreamsCocoa
 
-struct ConcatenateTransforms: View {
-    let sourceRect = CGRect(x: 120, y: 20, width: 500, height: 500)
-    let targetRect = CGRect(x: 20, y: 400, width: 100, height: 100)
+struct World {
+    let sourceRect = CGRect(x: 320, y: 20, width: 500, height: 500)
+    let targetRect = CGRect(x: 50, y: 300, width: 100, height: 100)
 
-    /// From [https://math.stackexchange.com/a/3249175/272082]:
-    ///
-    /// > Rotation and scaling matrices are usually defined around the origin. To perform these transformations about an arbitrary point, you would translate the point about which the transformation is to occur to the origin, perform the transformation as normal, and then undo the translation. This process is then repeated for each transformation required.
-    ///
+    // From [https://math.stackexchange.com/a/3249175/272082]:
+    //
+    // > Rotation and scaling matrices are usually defined around the origin. To perform these transformations about an arbitrary point, you would translate the point about which the transformation is to occur to the origin, perform the transformation as normal, and then undo the translation. This process is then repeated for each transformation required.
+    //
+
     var tOffset:CGAffineTransform {
         let originOffsetX = sourceRect.minX
         let originOffsetY = sourceRect.minY
         return CGAffineTransform(translationX: originOffsetX, y: originOffsetY).inverted()
     }
 
+    // The actual "geometry transforms"
     var tTranslate:CGAffineTransform {
         let xOffset = targetRect.minX - sourceRect.minX
         let yOffset = targetRect.minY - sourceRect.minY
@@ -48,86 +50,195 @@ struct ConcatenateTransforms: View {
             ("set to origin", tOffset),
             ("scaled", tScale),
             ("translated", tTranslate),
-            ("unset from origin", tOffset.inverted())
+            ("unset from origin", tOffset.inverted()),
         ]
     }
     var sequenceEnd:Int { tSequence.count-1 }
+}
 
-    @State var transformStatus = "At Source"
-    @State var transformState = -1
-    @State var transform = CGAffineTransform.identity
+struct ViewPort: View {
+    let tAnimation:CGAffineTransform
+    let tViewPort:CGAffineTransform
+    let world:World
+
+    @GestureState var dragState = CGSize.zero
+    @State var viewState = CGSize.zero
+    var tView:CGAffineTransform {
+        let tX = viewState.width + dragState.width
+        let tY = viewState.height + dragState.height
+        return tViewPort.concatenating(CGAffineTransform(translationX: tX, y: tY))
+    }
 
     var body: some View {
-        let tRect = sourceRect.applying(transform)
+        let drag = DragGesture()
+            .updating($dragState) { (value, state, transaction) in
+                state = value.translation
+            }
+            .onEnded { value in
+                viewState.width += value.translation.width
+                viewState.height += value.translation.height
+            }
+
+        let sourceRect = world.sourceRect.applying(tView)
+        let targetRect = world.targetRect.applying(tView)
+        let tRect = world.sourceRect.applying(tAnimation.concatenating(tView))
+
+        GeometryReader { geoReader in
+            // Source
+            Rectangle()
+                .fill(Color.green)
+                .frame(width: sourceRect.size.width, height: sourceRect.size.height)
+                .position(x: sourceRect.midX, y: sourceRect.midY)
+
+            // Target
+            Rectangle()
+                .fill(Color.blue)
+                .frame(width: targetRect.size.width, height: targetRect.size.height)
+                .position(x: targetRect.midX, y: targetRect.midY)
+
+            // Transforming image/rect
+            Image("Grid")
+                .resizable()
+                .frame(width: tRect.size.width-3, height: tRect.size.height-3)  // don't bleed to edge
+                .position(x: tRect.midX, y: tRect.midY)
+
+            // Origin x-hairs
+            Path { path in
+                let origin = CGPoint(x: 0, y: 0).applying(tView)
+                let extentX = CGPoint(x: 200, y: 0).applying(tView)
+                let extentY = CGPoint(x: 0, y: 200).applying(tView)
+                path.move(to: origin)
+                path.addLine(to: extentX)
+                path.move(to: origin)
+                path.addLine(to: extentY)
+            }
+            .stroke(Color.black, lineWidth: 1)
+
+        } // GeometryReader
+        .gesture(drag)
+        .clipped()
+    }
+}
+
+
+struct ConcatenateTransforms: View {
+    @State var animationStatus = "At Source"
+    @State var animationState = -1
+    @State var tAnimation = CGAffineTransform.identity
+
+    var world = World()
+
+    var body: some View {
+        let tView1 = CGAffineTransform.identity
+        let tView2 = CGAffineTransform(scaleX: 0.5, y: 0.25)
+        let tView3 = CGAffineTransform(scaleX: 0.2, y: 0.2)
+            .concatenating(CGAffineTransform(translationX: 200, y: 200))
+
+        // wrong order, we can see the translation of 200 scaled down to 40
+        let tView4 = CGAffineTransform(translationX: 200, y: 200)
+            .concatenating(CGAffineTransform(scaleX: 0.2, y: 0.2))
+
         VStack {
-            GeometryReader { _ in
-                // Source
-                Rectangle()
-                    .fill(Color.green)
-                    .frame(width: sourceRect.size.width, height: sourceRect.size.height)
-                    .position(x: sourceRect.midX, y: sourceRect.midY)
+            ViewPort(tAnimation: tAnimation, tViewPort: tView1, world: world)
+                .background(Color.gray)
 
-                // Target
-                Rectangle()
-                    .fill(Color.blue)
-                    .frame(width: targetRect.size.width, height: targetRect.size.height)
-                    .position(x: targetRect.midX, y: targetRect.midY)
+            HStack {
+                ViewPort(tAnimation: tAnimation, tViewPort: tView2, world: world)
+                    .background(Color.gray)
 
-                // Transforming image/rect
-                Image("Grid")
-                    .resizable()
-                    .frame(width: tRect.size.width-3, height: tRect.size.height-3)  // don't bleed to edge
-                    .position(x: tRect.midX, y: tRect.midY)
-            } // GeometryReader
-            .background(Color.gray)
+                VStack {
+                    ViewPort(tAnimation: tAnimation, tViewPort: tView3, world: world)
+                        .background(Color.gray)
+                    ViewPort(tAnimation: tAnimation, tViewPort: tView4, world: world)
+                        .background(Color.gray)
+                }
+            }
 
             HStack {
                 Spacer()
 
-                Button(action: { transformState -= 1 }) {
+                Button(action: { animationState -= 1 }) {
                     Image(systemName: "arrow.left.circle")
                         .font(.system(size: 56.0))
                 }
-                .disabled(transformState == sequenceStart)
+                .disabled(animationState == world.sequenceStart)
 
                 Spacer()
-                Text("\(transformStatus)")
+                Text("\(animationStatus)")
                     .font(.system(size: 40))
                     .frame(minWidth:600)
                 Spacer()
 
-                Button(action: { transformState += 1 }) {
+                Button(action: { animationState += 1 }) {
                     Image(systemName: "arrow.right.circle")
                         .font(.system(size: 56.0))
                 }
-                .disabled(transformState == sequenceEnd)
+                .disabled(animationState == world.sequenceEnd)
 
                 Spacer()
             } // HStack
+
         } // VStack
-        .onChange(of: transformState) { [transformState] newState in
-            let oldState = transformState
+        .onChange(of: animationState) { [animationState] newState in
+            let oldState = animationState
             if (newState > oldState) {
-                let sequence = tSequence[newState]  // apply new state
-                transformStatus = sequence.s
-                transform = transform.concatenating(sequence.t)
+                let sequence = world.tSequence[newState]  // apply new state
+                animationStatus = sequence.s
+                tAnimation = tAnimation.concatenating(sequence.t)
             } else {
-                let sequence = tSequence[oldState]  // undo old state
-                transformStatus = "un-" + sequence.s
-                transform = transform.concatenating(sequence.t.inverted())
+                let sequence = world.tSequence[oldState]  // undo old state
+                animationStatus = "un-" + sequence.s
+                tAnimation = tAnimation.concatenating(sequence.t.inverted())
             }
 
-            print("\(transformStatus)")
-            transform.print()
+//            print("\(animationStatus)")
+//            tAnimation.print()
 
-            if (newState == sequenceStart) {
-                transformStatus += " / At Source"
-            } else if (newState == sequenceEnd) {
-                transformStatus += " / At Target"
+            if (newState == world.sequenceStart) {
+                animationStatus += " / At Source"
+            } else if (newState == world.sequenceEnd) {
+                animationStatus += " / At Target"
             }
         }
     }
 }
+
+//// Canvas
+//Rectangle()
+//    .fill(Color.white)
+//    .frame(width: canvasRect.size.width, height: canvasRect.size.height)
+//    .position(x: canvasRect.midX, y: canvasRect.midY)
+//    .onAppear {
+//        let outerRect = CGRect(size: canvasRect.size)
+//        print(outerRect)
+//        let innerRect = CGRect(size: geoReader.size)
+//        print(innerRect)
+//        let fittedRect = innerRect.fit(rect: outerRect)
+//        print(innerRect)
+//        let scale = fittedRect.size.width / innerRect.size.width
+//        print(scale)
+//        let tx = fittedRect.minX - outerRect.minX
+//        let ty = fittedRect.minY - outerRect.minY
+//
+//        var t = CGAffineTransform.identity
+//        t = t.concatenating(CGAffineTransform(scaleX: scale, y: scale))
+//        t = t.concatenating(CGAffineTransform(translationX: tx, y: ty))
+//        t = t.concatenating(CGAffineTransform(translationX: canvasRect.minX, y: canvasRect.minY))
+//
+//        tCanvas = t
+//        geoRect = CGRect(size: geoReader.size).applying(tCanvas)
+//    }
+//
+//// GeoRect
+//Rectangle()
+//    .fill(Color.gray)
+//    .frame(width: geoRect.size.width, height: geoRect.size.height)
+//    .position(x: geoRect.midX, y: geoRect.midY)
+//drawTickMark("tl", ┌canvasRect)
+//    .stroke(Color.red, lineWidth: 2)
+//drawTickMark("br", ┘canvasRect)
+//    .stroke(Color.yellow, lineWidth: 2)
+
 
 /// https://stackoverflow.com/a/39215372/246801
 extension String {
@@ -166,4 +277,35 @@ extension CGAffineTransform {
         let s = "┌ \(a) \(b)  0 ┐\n" + "│ \(c) \(d)  0 │\n" + "└ \(tx) \(ty)  1 ┘\n"
         Swift.print(s)
     }
+}
+
+func drawTickMark(_ corner:String, _ origin:CGPoint, size:Int=20) -> Path {
+    let primeX:CGFloat
+    let primeY:CGFloat
+
+    let size = CGFloat(size)
+    switch corner {
+    case "tl":
+        primeX = origin.x + size
+        primeY = origin.y + size
+
+    case "br":
+        primeX = origin.x - size
+        primeY = origin.y - size
+
+    default:
+        primeX = origin.x
+        primeY = origin.y
+    }
+
+    let extentX = CGPoint(x: primeX, y: origin.y)
+    let extentY = CGPoint(x: origin.x, y: primeY)
+
+    var path = Path()
+    path.move(to: origin)
+    path.addLine(to: extentX)
+    path.move(to: origin)
+    path.addLine(to: extentY)
+
+    return path
 }
