ec982bfb8dde76c975ee52e32dbf6dac990f89d1
Zach Young <zacharysyoung@gmail.com>
2021-04-07 15:12:56 -0700

    working and looking good; want to add minimum size to AdjustableRegion

--- SwiftExplorer/Explorations/ClampingDragGesture.swift
+++ SwiftExplorer/Explorations/ClampingDragGesture.swift
@@ -10,43 +10,58 @@ import SwiftUI
 import hexdreamsCocoa
 
 class ImageAndRegion: ObservableObject {
-    @Published var regionOfInterest = CGRect(x: 20, y: 20, width: 50, height: 50)
-    let image = UIImage(named: "ChannelImageDefault")!
+    let name:String
+    let image:UIImage
+    @Published var regionOfInterest:CGRect
+
+    var subImage:UIImage {
+        let drawImage = image.cgImage!.cropping(to: self.regionOfInterest)
+        return UIImage(cgImage: drawImage!)
+    }
+
+    init(_ name:String, _ region:CGRect?) {
+        if let region = region {
+            self.regionOfInterest = region
+        } else {
+            self.regionOfInterest = CGRect(x: 20, y: 20, width: 50, height: 50)
+        }
+
+        self.name = name
+        self.image = UIImage(named: name)!
+    }
 
     var boundsX:CGFloat { image.size.width }
     var boundsY:CGFloat { image.size.height }
 
+    // Use target size and adjust the origin
     func moveTo(target: CGRect) {
-        var minX = target.minX
-        var minY = target.minY
-        var maxX = target.maxX
-        var maxY = target.maxY
+        let x:CGFloat
+        let y:CGFloat
+        let width = target.width
+        let height = target.height
 
-        if (minX < 0) {
-            minX = 0
-            maxX = target.width
+        if (target.minX < 0) {
+            x = 0
+        } else if (target.maxX > boundsX) {
+            x = boundsX - width
+        } else {
+            x = target.minX
         }
 
-        if (maxX > boundsX) {
-            minX = boundsX - target.width
-            maxX = boundsX
+        if (target.minY < 0) {
+            y = 0
+        } else if (target.maxY > boundsY) {
+            y = boundsY - height
+        } else {
+            y = target.minY
         }
 
-        if (minY < 0) {
-            minY = 0
-            maxY = target.height
-        }
-
-        if (maxY > boundsY) {
-            minY = boundsY - target.height
-            maxY = boundsY
-        }
-
-        let clampedTarget = CGRect(x: minX, y: minY, width: maxX-minX, height: maxY-minY)
+        let clampedTarget = CGRect(x: x, y: y, width: width, height: height)
         self.regionOfInterest = clampedTarget
         print("moveTo >> regionOfInterest: \(self.regionOfInterest)")
     }
 
+    // Adjust any point
     func resizeTo(target: CGRect) {
         let minX = (target.minX < 0)       ? 0       : target.minX
         let minY = (target.minY < 0)       ? 0       : target.minY
@@ -59,8 +74,55 @@ class ImageAndRegion: ObservableObject {
     }
 }
 
+extension ImageAndRegion: Equatable {
+    static func == (lhs: ImageAndRegion, rhs: ImageAndRegion) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
+class Controller: ObservableObject {
+    let iAndRs = [
+        ImageAndRegion(
+            "Comic3",
+            CGRect(x: 1927.32, y: 1582.24, width: 612.38, height: 485.25)
+        ),
+        ImageAndRegion(
+            "Middle Earth",
+            CGRect(x: 2327.01, y: 2189.8, width: 609.67, height: 410.5)
+        )
+    ]
+
+    @Published var selected:ImageAndRegion
+
+    init() {
+        self.selected = iAndRs[0]
+    }
+}
+
 struct ClampingDragGesture: View {
-    let iAndR = ImageAndRegion()
+    @ObservedObject var controller = Controller()
+
+    var body: some View {
+        NavigationView {
+            List(controller.iAndRs, id: \.name) { iAndR in
+                HStack {
+                    Button("\(iAndR.name)") {
+                        controller.selected = iAndR
+                    }
+                    if (iAndR == controller.selected) {
+                        Image(systemName: "checkmark.circle")
+                    }
+                }
+            }
+            .listStyle(SidebarListStyle())
+
+            DetailView(iAndR: controller.selected)
+        }
+    }
+}
+
+struct DetailView: View {
+    @ObservedObject var iAndR:ImageAndRegion
 
     var body: some View {
         let image = self.iAndR.image
@@ -73,12 +135,17 @@ struct ClampingDragGesture: View {
                 Image(uiImage: image)
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                    .border(Color.red, width: 2)
                     .position(x: imageFrame.midX, y: imageFrame.midY)
                     .frame(width: imageFrame.width, height: imageFrame.height)
 
-                AdjustableRegion(iAndR: iAndR, tRegion: tFitImage)
-            }
-        }
+                AdjustableRegion(iAndR: self.iAndR, tRegion: tFitImage)
+            }  // GeometryReader
+
+            Image(uiImage: iAndR.subImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+        }  // VStack
     }
 
     /// Return a transform that fits a size (centered and scaled) in a frame
@@ -90,7 +157,7 @@ struct ClampingDragGesture: View {
         let t = CGAffineTransform.identity
             .concatenating(CGAffineTransform(scaleX: scale, y: scale))
             .concatenating(CGAffineTransform(translationX: offset.x, y: offset.y))
-//        print("tFitSizeInFrame >> t: \(t)")
+        //        print("tFitSizeInFrame >> t: \(t)")
         return t
     }
 }
@@ -99,116 +166,124 @@ struct AdjustableRegion: View {
     @ObservedObject var iAndR:ImageAndRegion
     let tRegion:CGAffineTransform
 
+    // Copy last UI region before adjustment to reference during adjustment
     @State var originalRegion = CGRect.zero
 
     var body: some View {
         let region = iAndR.regionOfInterest.applying(tRegion)
+
+        // Region's outline
+        Rectangle()
+            .stroke(Color.blue, lineWidth: 2)
+            .shadow(color: .white, radius: 5)
+            .position(x: region.midX, y: region.midY)
+            .frame(width: region.width, height: region.height)
+
+        // All interactive/adjustment UI
+
+        drawResizeHandlesFor(region, "tl")
+            .gesture(combined(region) { gesture in
+                let o = originalRegion
+                let tx = gesture.translation.width
+                let ty = gesture.translation.height
+                let new = CGRect(x: o.minX+tx, y: o.minY+ty, width: o.width+(tx * -1), height: o.height+(ty * -1))
+                iAndR.resizeTo(target: new.applying(tRegion.inverted()))
+            })
+
+        drawMoveHandlesFor(region)
+            .gesture(combined(region) { gesture in
+                let o = originalRegion
+                let tx = gesture.translation.width
+                let ty = gesture.translation.height
+                let new = CGRect(x: o.minX+tx, y: o.minY+ty, width: o.width, height: o.height)
+                iAndR.moveTo(target: new.applying(tRegion.inverted()))
+            })
+
+        drawResizeHandlesFor(region, "br")
+            .gesture(combined(region) { gesture in
+                let o = originalRegion
+                let tx = gesture.translation.width
+                let ty = gesture.translation.height
+                let new = CGRect(x: o.minX, y: o.minY, width: o.width+tx, height: o.height+ty)
+                iAndR.resizeTo(target: new.applying(tRegion.inverted()))
+            })
+
+    }
+
+    typealias AdjustSourceOfTruthGesture = SequenceGesture<_EndedGesture<LongPressGesture>, _EndedGesture<_ChangedGesture<DragGesture>>>
+
+    // Take the UI region to copy/save before drag, and a closure that adjusts the region's SOT
+    func combined(_ region: CGRect, f: @escaping (DragGesture.Value)->Void) -> AdjustSourceOfTruthGesture {
         let longPress = LongPressGesture(minimumDuration: 0.0)
             .onEnded { _ in
                 self.originalRegion = region
             }
 
-        // Top left - resize
+        return longPress.sequenced(
+            before: DragGesture(minimumDistance: 0, coordinateSpace: .local)
+                .onChanged { gesture in
+                    f(gesture)
+                }
+                .onEnded { _ in
+                    self.originalRegion = CGRect.zero
+                }
+        )
+    }
+
+    func drawMoveHandlesFor(_ region: CGRect) -> some View {
         Group {
             Rectangle()
                 .fill(Color.blue.opacity(0.125))
-                .shadow(color: .white, radius: 5)
-                .position(x: region.minX+5, y: region.minY+5)
-                .frame(width: 20, height: 20)
-
-        Image(systemName: "chevron.left")
-            .font(.system(size: 50))
-            .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-            .shadow(color: .white, radius: 5)
-            .rotationEffect(.init(degrees: 45))
-            .position(x: region.minX, y: region.minY)
-        }
-            .gesture(
-                longPress.sequenced(
-                    before: DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                        .onChanged { gesture in
-                            let o = originalRegion
-                            let tx = gesture.translation.width
-                            let ty = gesture.translation.height
-                            let new = CGRect(x: o.minX+tx, y: o.minY+ty, width: o.width+(tx * -1), height: o.height+(ty * -1))
-                            iAndR.resizeTo(target: new.applying(tRegion.inverted()))
-                        }
-                        .onEnded { _ in
-                            self.originalRegion = CGRect.zero
-                        }
-                ))
-
-        Rectangle()
-            .stroke(Color.blue, lineWidth: 4)
-            .shadow(color: .white, radius: 5)
-            .position(x: region.midX, y: region.midY)
-            .frame(width: region.width, height: region.height)
-
-
-        // Center - move
-        Group {
-            Rectangle()
-                .fill(Color.blue.opacity(0.125))
-                .shadow(color: .white, radius: 5)
+//                .shadow(color: .white, radius: 5)
                 .rotationEffect(.init(degrees: 45))
                 .position(x: region.midX, y: region.midY)
                 .frame(width: 30, height: 30)
             ForEach(0..<4) { idx in
                 Image(systemName: "chevron.up")
-                    .font(.system(size: 20, weight: .heavy))
+                    .font(.system(size: 20, weight: .bold))
                     .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                    .shadow(color: .white, radius: 5)
+//                    .shadow(color: .white, radius: 5)
                     .offset(y:-25)
                     .rotationEffect(.init(degrees: 90 * Double(idx)))
                     .position(x: region.midX, y: region.midY)
             }
         }
-        .gesture(
-            longPress.sequenced(
-                before: DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                    .onChanged { gesture in
-                        let o = originalRegion
-                        let tx = gesture.translation.width
-                        let ty = gesture.translation.height
-                        let new = CGRect(x: o.minX+tx, y: o.minY+ty, width: o.width, height: o.height)
-                        iAndR.moveTo(target: new.applying(tRegion.inverted()))
-                    }
-                    .onEnded { _ in
-                        self.originalRegion = CGRect.zero
-                    }
-            ))
+    }
 
-        // Bottom right - resize
-        Group {
-            Rectangle()
-                .fill(Color.blue.opacity(0.125))
-                .shadow(color: .white, radius: 5)
-                .position(x: region.maxX-5, y: region.maxY-5)
-                .frame(width: 20, height: 20)
+    func drawResizeHandlesFor(_ region: CGRect, _ corner:String) -> some View {
+        let x:CGFloat, y:CGFloat
+        let name:String
 
-            Image(systemName: "chevron.right")
-                .font(.system(size: 50))
-                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-                .shadow(color: .white, radius: 5)
-                .rotationEffect(.init(degrees: 45))
-                .position(x: region.maxX, y: region.maxY)
+        switch corner {
+        case "tl":
+            name = "chevron.left"
+            x = region.minX-2
+            y = region.minY-2
+        case "br":
+            name = "chevron.right"
+            x = region.maxX+2
+            y = region.maxY+2
+        default:
+            x = 0
+            y = 0
+            name = ""
         }
-        .gesture(
-            longPress.sequenced(
-                before: DragGesture(minimumDistance: 0, coordinateSpace: .local)
-                    .onChanged { gesture in
-                        let o = originalRegion
-                        let tx = gesture.translation.width
-                        let ty = gesture.translation.height
-                        let new = CGRect(x: o.minX, y: o.minY, width: o.width+tx, height: o.height+ty)
-                        iAndR.resizeTo(target: new.applying(tRegion.inverted()))
-                    }
-                    .onEnded { _ in
-                        self.originalRegion = CGRect.zero
-                    }
-            ))
 
+        return Group {
+            // A mostly transparent Shape to give the user something to tap on
+            Rectangle()
+                .fill(Color.white.opacity(0.125))
+//                .shadow(color: .white, radius: 5)
+                .position(x: x, y: y)
+                .frame(width: 15, height: 15)
 
+            Image(systemName: name)
+                .font(.system(size: 20, weight: .bold))
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+//                .shadow(color: .white, radius: 5)
+                .rotationEffect(.init(degrees: 45))
+                .position(x: x, y: y)
+        }
     }
 }
 
